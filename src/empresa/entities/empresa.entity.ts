df6bb06964ad2afaa2d494ffbe373e0c870c26c1
leoAlvTor <calvaradot1@est.ups.edu.ec>
2023-01-08 17:07:51 -0500

    empresa.entity.ts new fields for Empresa entity. package.json and yarn.lock with new dependency 'mongoose'.

--- src/empresa/entities/empresa.entity.ts
+++ src/empresa/entities/empresa.entity.ts
@@ -1,1 +1,33 @@
-export class Empresa {}
+import { Document } from "mongoose"
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+
+@Schema()
+export class Empresa extends Document{
+
+    @Prop({
+        unique: true,
+        index: true,
+    })
+    nombre: string;
+
+    @Prop({
+        unique: true,
+        index: true,
+    })
+    direccion: string;
+
+    @Prop({
+        unique: true,
+        index: true,
+    })
+    telefono: string;
+
+    @Prop({
+        unique: true,
+        index: true,
+    })
+    email: string;
+
+}
+
+export const EmpresaSchema = SchemaFactory.createForClass(Empresa);
