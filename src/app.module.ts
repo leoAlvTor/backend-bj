a95bf1252b731658b36eab4a0040d8835d734f88
leoAlvTor <calvaradot1@est.ups.edu.ec>
2023-01-08 20:52:27 -0500

    Changes on App Module and Empresa

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,11 +5,6 @@ import { ProductoModule } from './producto/producto.module';
 import {MongooseModule} from "@nestjs/mongoose";
 
 @Module({
-  imports: [
-
-      MongooseModule.forRoot('mongodb://localhost:27017/backend'),
-
-      UsuarioModule, EmpresaModule, ProductoModule
-  ],
+  imports: [UsuarioModule, EmpresaModule, ProductoModule],
 })
 export class AppModule {}
--- src/empresa/dto/create-empresa.dto.ts
+++ src/empresa/dto/create-empresa.dto.ts
@@ -1,14 +1,1 @@
-import {IsString} from "class-validator";
-
-export class CreateEmpresaDto {
-
-    @IsString()
-    nombre: string;
-    @IsString()
-    direccion: string;
-    @IsString()
-    telefono: string;
-    @IsString()
-    email: string;
-
-}
+export class CreateEmpresaDto {}
