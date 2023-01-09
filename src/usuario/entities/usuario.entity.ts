1a9fefcf00c1ff500583bb6cf1e532a73892bd2a
Braulio Astudillo <brau97x@gmail.com>
2023-01-08 22:00:46 -0500

    schemaUsuario

--- src/usuario/entities/usuario.entity.ts
+++ src/usuario/entities/usuario.entity.ts
@@ -1,3 +1,59 @@
-export class Usuario {
+import { Schema } from '@nestjs/mongoose'
+import { Prop, SchemaFactory } from '@nestjs/mongoose/dist';
+import { Document } from "mongoose";
+export class Usuario extends Document {
+    @Prop({
+        unique:true,
+        index:true,
+    })
+    cedula:string
+
+    @Prop({
+        index:true,
+    })
     nombre:string
+
+    @Prop({
+        index:true,
+    })
+    apellidos:string
+
+    @Prop({
+        index:true,
+    })
+    telefono:string
+
+    @Prop({
+        index:true,
+    })
+    direcciom: string
+
+    @Prop({
+        index:true,
+    })
+    fechaNac: Date
+
+    @Prop({
+        index:true,
+    })
+    Rol:string
+
+    @Prop({
+        index:true,
+    })
+    email:string
+
+    @Prop({
+        index:true,
+    })
+    contrasenia:string
+
+    @Prop({
+        index:true,
+    })
+    sexo:string
+
 }
+
+
+export const UsuarioSchema = SchemaFactory.createForClass(Usuario);
