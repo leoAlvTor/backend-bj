4ae6911683f9164a110108d5dc7189f77dd9c33a
Braulio Astudillo <brau97x@gmail.com>
2023-01-08 22:12:54 -0500

    eroor

--- src/usuario/usuario.module.ts
+++ src/usuario/usuario.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { UsuarioService } from './usuario.service';
+import { MongooseModule } from '@nestjs/mongoose';
 import { UsuarioController } from './usuario.controller';
+import { Usuario, UsuarioSchema } from './entities/usuario.entity';
 
 @Module({
   controllers: [UsuarioController],
-  providers: [UsuarioService]
+  providers: [UsuarioService],
+  imports:[
+    MongooseModule.forFeature([
+      {
+        name: Usuario.name,
+        schema: UsuarioSchema,
+      }
+    ])
+  ]
 })
 export class UsuarioModule {}
+
+
+
+
+
