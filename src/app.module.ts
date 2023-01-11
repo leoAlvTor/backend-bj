1c5424544dfbfd949ed88e67d1de92dc562f8654
leoAlvTor <calvaradot1@est.ups.edu.ec>
2023-01-10 21:25:16 -0500

    Pedido Changes (Global change on App.module.ts)

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { UsuarioModule } from './usuario/usuario.module';
 import { EmpresaModule } from './empresa/empresa.module';
 import { ProductoModule } from './producto/producto.module';
 import {MongooseModule} from "@nestjs/mongoose";
+import { PedidoModule } from './pedido/pedido.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost:27017/backend'),
-    UsuarioModule, EmpresaModule, ProductoModule],
+    UsuarioModule, EmpresaModule, ProductoModule, PedidoModule],
 })
 export class AppModule {}
