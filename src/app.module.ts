cd6fa07c2ba7e896a613a24eca776b0932f28040
Braulio Astudillo <brau97x@gmail.com>
2023-01-11 21:27:53 -0500

    moduloLogin

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,11 @@ import { EmpresaModule } from './empresa/empresa.module';
 import { ProductoModule } from './producto/producto.module';
 import {MongooseModule} from "@nestjs/mongoose";
 import { PedidoModule } from './pedido/pedido.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost:27017/backend'),
-    UsuarioModule, EmpresaModule, ProductoModule, PedidoModule],
+    UsuarioModule, EmpresaModule, ProductoModule, PedidoModule, AuthModule],
 })
 export class AppModule {}
